dd0c0045786766c4ed9691ee2807b06d9f165901
kareemelbalshe <kareemelbalshe1234@gmail.com>
2025-04-21 19:58:47 +0200

    mange header logic

--- dashboard/seller-dashboard/seller-products/main.js
+++ dashboard/seller-dashboard/seller-products/main.js
@@ -2,7 +2,7 @@ import { logout } from "../../shared/Api.js";
 
 window.addEventListener("load", () => {
     if (localStorage.getItem("isLoggedIn") !== "true" && localStorage.getItem("isSeller") !== "true") {
-        window.location.href = "../index.html";
+        window.location.href = "../../index.html";
     }
 });
 
@@ -10,5 +10,5 @@ const logoutBtn = document.getElementById("logout");
 
 logoutBtn.addEventListener("click", () => {
     logout();
-    window.location.href = "../index.html";
+    window.location.href = "../../index.html";
 });
