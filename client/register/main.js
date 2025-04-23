fc960942863bc5bba6394085024fe3e3bca70a42
kareemelbalshe <kareemelbalshe1234@gmail.com>
2025-04-23 21:41:54 +0200

    fix register

---
+++ client/register/main.js
@@ -0,0 +1,37 @@
+import { registerCustomer } from "../../shared/Api.js";
+import createId from "../../shared/createId.js";
+import getCurrentTimestamp from "../../shared/setTime.js";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const id = createId();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const phone = document.getElementById("phone").value;
+  const address = document.getElementById("Address").value;
+  const createdAt = getCurrentTimestamp();
+
+  if (password !== confirmPassword) {
+    alert("Passwords don't match!");
+    return;
+  }
+
+  const newCustomer = {
+    id,
+    name,
+    email,
+    password,
+    phone,
+    address,
+    numBuys: 0,
+    totalSpent: 0,
+    createdAt,
+  };
+
+  await registerCustomer(newCustomer);
+});
