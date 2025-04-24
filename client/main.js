55e55b83e7ee04c5f4316caab1c0bb4472cca1cd
kareemelbalshe <kareemelbalshe1234@gmail.com>
2025-04-24 17:00:17 +0200

    but module

--- client/main.js
+++ client/main.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const cartLink = document.createElement("a");
     cartLink.className = "btn btn-outline-dark me-2";
-    cartLink.href = "../cart/index.html";
+    cartLink.href = "./cart/index.html";
     cartLink.innerHTML = '<i class="bi bi-cart4"></i>';
 
     headerActions.appendChild(logoutBtn);
